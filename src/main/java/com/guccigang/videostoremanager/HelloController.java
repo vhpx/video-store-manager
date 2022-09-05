ee56711225209b3ca9ca33ed6f5a666ac55cd567
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-09-05 12:46:22 +0700

    Initialize project ưith JavaFX

---
+++ src/main/java/com/guccigang/videostoremanager/HelloController.java
@@ -0,0 +1,14 @@
+package com.guccigang.videostoremanager;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
